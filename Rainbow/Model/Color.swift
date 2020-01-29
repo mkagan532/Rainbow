0897de9d8230453a32230c25ce3d76fdbbfb1ff8
Michael Kagan <mkagan532@gmail.com>
2020-01-28 23:03:37 -0500

    Set up tableview with colors array

---
+++ Rainbow/Model/Color.swift
@@ -0,0 +1,9 @@
+//
+//  Color.swift
+//  Rainbow
+//
+//  Created by Lambda_School_Loaner_232 on 1/28/20.
+//  Copyright © 2020 Michael Kagan. All rights reserved.
+//
+
+import Foundation
---
+++ Rainbow/View Controllers/ColorDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ColorDetailsViewController.swift
+//  Rainbow
+//
+//  Created by Lambda_School_Loaner_232 on 1/28/20.
+//  Copyright © 2020 Michael Kagan. All rights reserved.
+//
+
+import UIKit
+
+class ColorDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
